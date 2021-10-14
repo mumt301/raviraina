7d8fe5d4890b91b31a9a7062067a7df039f9606a
Ravi Raina <ravi.raina@brentwood.ca>
2021-10-13 20:08:50 -0400

    a4 javascript, first sample data added

---
+++ A4/script.js
@@ -0,0 +1,225 @@
+// JSON Objects for Guitar-Blues Sample
+let blues = [{
+        "Filename": "Blues",
+        "Compression Algorithm": "MP3",
+        "Bit-Rate": "96kbps",
+        "Original Size": 782,
+        "Compressed SIze": 112,
+        "Compression Ratio": 0.143222506393862,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/mp3/blues_mp3_96kbps.mp3",
+        "Residual Audio": "resources/blues/mp3/blues_mp3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "MP3",
+        "Bit-Rate": "128kbps",
+        "Original Size": 782,
+        "Compressed SIze": 140,
+        "Compression Ratio": 0.179028132992327,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/mp3/blues_mp3_128kbps.mp3",
+        "Residual Audio": "resources/blues/mp3/blues_mp3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "MP3",
+        "Bit-Rate": "192kbps",
+        "Original Size": 782,
+        "Compressed SIze": 210,
+        "Compression Ratio": 0.268542199488491,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/mp3/blues_mp3_192kbps.mp3",
+        "Residual Audio": "resources/blues/mp3/blues_mp3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "MP3",
+        "Bit-Rate": "320kbps",
+        "Original Size": 782,
+        "Compressed SIze": 318,
+        "Compression Ratio": 0.406649616368286,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/mp3/blues_mp3_320kbps.mp3",
+        "Residual Audio": "resources/blues/mp3/blues_mp3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "M4A",
+        "Bit-Rate": "98kbps",
+        "Original Size": 782,
+        "Compressed SIze": 246,
+        "Compression Ratio": 0.31457800511509,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/m4a/blues_m4a_98kbps.m4a",
+        "Residual Audio": "resources/blues/m4a/blues_m4a_98kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "M4A",
+        "Bit-Rate": "128kbps",
+        "Original Size": 782,
+        "Compressed SIze": 247,
+        "Compression Ratio": 0.315856777493606,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/m4a/blues_m4a_128kbps.m4a",
+        "Residual Audio": "resources/blues/m4a/blues_m4a_98kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "M4A",
+        "Bit-Rate": "192kbps",
+        "Original Size": 782,
+        "Compressed SIze": 248,
+        "Compression Ratio": 0.317135549872123,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/m4a/blues_m4a_192kbps.m4a",
+        "Residual Audio": "resources/blues/m4a/blues_m4a_98kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "M4A",
+        "Bit-Rate": "320kbps",
+        "Original Size": 782,
+        "Compressed SIze": 248,
+        "Compression Ratio": 0.317135549872123,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/m4a/blues_m4a_320kbps.m4a",
+        "Residual Audio": "resources/blues/m4a/blues_m4a_98kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "AC3",
+        "Bit-Rate": "96kbps",
+        "Original Size": 782,
+        "Compressed SIze": 107,
+        "Compression Ratio": 0.136828644501279,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ac3/blues_ac3_96kbps.m4a",
+        "Residual Audio": "resources/blues/ac3/blues_ac3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "AC3",
+        "Bit-Rate": "128kbps",
+        "Original Size": 782,
+        "Compressed SIze": 142,
+        "Compression Ratio": 0.181585677749361,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ac3/blues_ac3_128kbps.m4a",
+        "Residual Audio": "resources/blues/ac3/blues_ac3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "AC3",
+        "Bit-Rate": "192kbps",
+        "Original Size": 782,
+        "Compressed SIze": 213,
+        "Compression Ratio": 0.272378516624041,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ac3/blues_ac3_192kbps.m4a",
+        "Residual Audio": "resources/blues/ac3/blues_ac3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "AC3",
+        "Bit-Rate": "320kbps",
+        "Original Size": 782,
+        "Compressed SIze": 355,
+        "Compression Ratio": 0.453964194373402,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ac3/blues_ac3_320kbps.m4a",
+        "Residual Audio": "resources/blues/ac3/blues_ac3_96kbps_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "OGG",
+        "Bit-Rate": "Low",
+        "Original Size": 782,
+        "Compressed SIze": 69,
+        "Compression Ratio": 0.0882352941176471,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ogg/blues_ogg_low.ogg",
+        "Residual Audio": "resources/blues/ogg/blues_ogg_low_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "OGG",
+        "Bit-Rate": "Low-Mid",
+        "Original Size": 782,
+        "Compressed SIze": 79,
+        "Compression Ratio": 0.101023017902813,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ogg/blues_ogg_midlow.ogg",
+        "Residual Audio": "resources/blues/ogg/blues_ogg_low_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "OGG",
+        "Bit-Rate": "High-Mid",
+        "Original Size": 782,
+        "Compressed SIze": 145,
+        "Compression Ratio": 0.18542199488491,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ogg/blues_ogg_midhigh.ogg",
+        "Residual Audio": "resources/blues/ogg/blues_ogg_low_residual.wav"
+    },
+    {
+        "Filename": "Blues",
+        "Compression Algorithm": "OGG",
+        "Bit-Rate": "High",
+        "Original Size": 782,
+        "Compressed SIze": 272,
+        "Compression Ratio": 0.347826086956522,
+        "Original Audio": "resources/blues/blues.wav",
+        "Compressed Audio": "resources/blues/ogg/blues_ogg_high.ogg",
+        "Residual Audio": "resources/blues/ogg/blues_ogg_low_residual.wav"
+    }
+]
+
+// function to create and display media table
+function create_media_table(json_vals) {
+    let filename = json_vals[0]['Filename']
+
+    // only create elements once
+    if (document.getElementById(filename)) {
+        console.log("Already Created")
+    } else {
+
+        let table = document.createElement('table');
+        table.id = filename
+
+        // create header
+        let header = Object.keys(json_vals[0]);
+        let tr = table.insertRow(-1);
+        for (let i = 0; i < header.length; i++) {
+            let th = document.createElement("th");
+            th.innerHTML = header[i];
+            tr.appendChild(th);
+        }
+
+        // create table
+        for (row = 0; row < json_vals.length; row++) {
+            tr = document.createElement('tr');
+            let columns = Object.keys(json_vals[row]);
+
+            for (col = 0; col < columns.length; col++) {
+
+                td = document.createElement('td');
+                tr.appendChild(td);
+                if (col < 6) {
+                    td.innerHTML = json_vals[row][columns[col]];
+                } else {
+                    a = document.createElement('a');
+                    a.href = json_vals[row][columns[col]];
+                    a.innerHTML = "Click Here";
+                    td.appendChild(a)
+                }
+
+            }
+            table.appendChild(tr);
+        }
+        document.getElementById("discography_table").appendChild(table);
+    }
+
+}
